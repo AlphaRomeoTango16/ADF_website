085a9aca75dcb0347b9b1e9ee06683f6e51794ce
Arthur Della Faille <arthur16.dellafaille@gmail.com>
2022-06-09 16:22:13 +0200

    modifications

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -16,6 +16,25 @@ import MySQLLogo from '../../assets/logo/MySQL.svg'
 import ExpressJsLogo from '../../assets/logo/expressjs.svg'
 import NodemonLogo from '../../assets/logo/nodemon.svg'
 import SequelizeLogo from '../../assets/logo/sequelize.svg'
+import AirtableLogo from '../../assets/logo/airtable.svg'
+import AxiosLogo from '../../assets/logo/axios.svg'
+import DiagramsLogo from '../../assets/logo/diagrams.svg'
+import FigmaLogo from '../../assets/logo/figma.svg'
+import FirebaseLogo from '../../assets/logo/firebase.svg'
+import GitHubLogo from '../../assets/logo/github.svg'
+import GitLabLogo from '../../assets/logo/gitlab.svg'
+import I18nextLogo from '../../assets/logo/i18next.svg'
+import MapBoxLogo from '../../assets/logo/mapbox.svg'
+import MaterialUiLogo from '../../assets/logo/materialui.svg'
+import MomentjsLogo from '../../assets/logo/momentjs.svg'
+import NetlifyLogo from '../../assets/logo/netlify.svg'
+import NextjsLogo from '../../assets/logo/nextjs.svg'
+import NotionLogo from '../../assets/logo/notion.svg'
+import PostmanLogo from '../../assets/logo/postman.svg'
+import ReactHookFormLogo from '../../assets/logo/reacthookform.svg'
+import ReduxLogo from '../../assets/logo/redux.svg'
+import RetoolLogo from '../../assets/logo/retool.svg'
+import TypescriptLogo from '../../assets/logo/typescript.svg'
 import { useTranslation } from 'react-i18next'
 import { Helmet } from 'react-helmet-async'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -216,6 +235,7 @@ const StackIcon = styled.div`
     height: 120px;
     border-top: none;
     transition: transform 1s;
+    overflow: scroll;
     transform: ${({isOpen}) => isOpen ? "translateY(0px)" : "translateY(-130px)"};
     @media screen and (max-width: 1200px) {
         width: 93%;
@@ -290,9 +310,17 @@ function About() {
                                         <a href="https://developer.mozilla.org/fr/docs/Web/CSS"><LogoTool src={CssLogo} title="CSS3"/></a>
                                         <a href="https://developer.mozilla.org/fr/docs/Web/JavaScript"><LogoTool src={JsLogo} title="Javascript"/></a>
                                         <a href="https://reactjs.org/"><LogoTool src={ReactLogo} title="ReactJs"/></a>
+                                        <a href="https://www.typescriptlang.org/"><LogoTool src={TypescriptLogo} title="Typescript"/></a>
+                                        <a href="https://nextjs.org/"><LogoTool src={NextjsLogo} title="NextJs"/></a>
                                         <a href="https://vuejs.org/"><LogoTool src={VueLogo} title="VueJs"/></a>
+                                        <a href="https://redux.js.org/"><LogoTool src={ReduxLogo} title="Redux"/></a>
                                         <a href="https://sass-lang.com/"><LogoTool src={SassLogo} title="Sass"/></a>
                                         <a href="https://getbootstrap.com/"><LogoTool src={BootstrapLogo} title="Bootstrap"/></a>
+                                        <a href="https://mui.com/"><LogoTool src={MaterialUiLogo} title="Material-Ui"/></a>
+                                        <a href="https://www.mapbox.com/"><LogoTool src={MapBoxLogo} title="MapBox"/></a>
+                                        <a href="https://momentjs.com/"><LogoTool src={MomentjsLogo} title="Moment"/></a>
+                                        <a href="https://www.i18next.com/"><LogoTool src={I18nextLogo} title="i18next"/></a>
+                                        <a href="https://react-hook-form.com/"><LogoTool src={ReactHookFormLogo} title="React-Hoo-Form"/></a>
                                     </StackIcon>
                                 </IconContainer>
                             </StackContainer>
@@ -301,7 +329,9 @@ function About() {
                                 <IconContainer>
                                     <StackIcon isOpen={backIcon === true}>
                                         <a href="https://nodejs.org/en/"><LogoTool src={NodeLogo} title="NodeJs"/></a>
+                                        <a href="https://firebase.google.com/"><LogoTool src={FirebaseLogo} title="Firebase"/></a>
                                         <a href="https://expressjs.com/fr/"><LogoTool src={ExpressJsLogo} title="ExpressJs"/></a>
+                                        <a href="https://axios-http.com/"><LogoTool src={AxiosLogo} title="Axios"/></a>
                                         <a href="https://www.npmjs.com/package/nodemon"><LogoTool src={NodemonLogo} title="Nodemon"/></a>
                                         <a href="https://sequelize.org/"><LogoTool src={SequelizeLogo} title="Sequelize"/></a>
                                         <a href="https://www.mongodb.com/fr-fr"><LogoTool src={MongoDBLogo} title="MongoDB"/></a>
@@ -313,8 +343,17 @@ function About() {
                                 <StackTitle onClick={showToolsIcon} isOpen={toolsIcon === true}>{t("Tools")}<ChevronIcon isOpen={toolsIcon === true} icon={['fas', 'chevron-up']}/></StackTitle>
                                 <IconContainer>
                                     <StackIcon isOpen={toolsIcon === true}>
-                                        <a href="https://git-scm.com/"><LogoTool src={GitLogo} title="Git"/></a>
                                         <a href="https://code.visualstudio.com/"><LogoTool src={VSLogo} title="Visual Studio Code"/></a>
+                                        <a href="https://git-scm.com/"><LogoTool src={GitLogo} title="Git"/></a>
+                                        <a href="https://github.com/"><LogoTool src={GitHubLogo} title="GitHub"/></a>
+                                        <a href="hhttps://about.gitlab.com/"><LogoTool src={GitLabLogo} title="GitLab"/></a>
+                                        <a href="https://www.figma.com/"><LogoTool src={FigmaLogo} title="Figma"/></a>
+                                        <a href="https://www.postman.com/"><LogoTool src={PostmanLogo} title="Postman"/></a>
+                                        <a href="https://www.netlify.com/"><LogoTool src={NetlifyLogo} title="Netlify"/></a>
+                                        <a href="https://retool.com/"><LogoTool src={RetoolLogo} title="Retool"/></a>
+                                        <a href="https://www.notion.so/"><LogoTool src={NotionLogo} title="Notion"/></a>
+                                        <a href="https://www.diagrams.net/"><LogoTool src={DiagramsLogo} title="Diagrams.net"/></a>
+                                        <a href="https://www.airtable.com/"><LogoTool src={AirtableLogo} title="Airtable"/></a>
                                     </StackIcon>
                                 </IconContainer>
                             </StackContainer>
